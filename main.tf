81927e028230bb68f9c5dc59dbd35fe546df3da3
VictorBarros <victorbarros1130@gmail.com>
2023-07-21 19:27:54 -0400

    add credential variables

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
-provider "cloudflare" {}
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
 
 resource "random_pet" "bucket" {
   length = 1
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "website_bucket_name" {
-  description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.site.id
-}
+# output "website_bucket_name" {
+#   description = "Name (id) of the bucket"
+#   value       = aws_s3_bucket.site.id
+# }
 
-output "bucket_endpoint" {
-  description = "Bucket endpoint"
-  value       = aws_s3_bucket.site.website_endpoint
-}
+# output "bucket_endpoint" {
+#   description = "Bucket endpoint"
+#   value       = aws_s3_bucket.site.website_endpoint
+# }
 
-output "cloudfront_endpoint" {
-  description = "Cloudfront endpoint"
-  value       = aws_cloudfront_distribution.dist.domain_name
-}
+# output "cloudfront_endpoint" {
+#   description = "Cloudfront endpoint"
+#   value       = aws_cloudfront_distribution.dist.domain_name
+# }
 
-output "domain_name" {
-  description = "Website endpoint"
-  value       = var.site_domain
-}
+# output "domain_name" {
+#   description = "Website endpoint"
+#   value       = var.site_domain
+# }
